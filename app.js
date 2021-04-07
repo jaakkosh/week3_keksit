8bf1d7dae4f607e164506cb1176cc3b333157815
Jaakkosh <jaakko.hotti3@gmail.com>
2021-04-07 10:48:50 +0300

    valmis?

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 'use strict';
 const express = require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const passport = require('./utils/pass');
 const session = require('express-session');
 const app = express();
 const port = 3000;
+
+
+// add following after const port = 3000;
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
+
+
 //älä tee näin projektissa (koko slide)
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -15,8 +28,13 @@ app.use(session({
   secret:'salaisuuksia',
   resave: false,
   saveUninitialized:true,
-  cookie: {maxAge: 3600}
+  cookie: {maxAge: 36000}
 }));
+
+// add following after app.use(session...
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -25,25 +43,17 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 
-app.get('/secret', (req, res) => {
-  if(req.session.kirjautunut) {
-    res.render('secret');
-  }else{
-    res.redirect('/form');
-  }
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 
-app.post('/login', (req, res) => {
-  const passwd = req.body.password;
-  const uname = req.body.username;
-  if(uname === username && passwd === password){
-    req.session.kirjautunut = true;
-    res.redirect('secret')
-  }
-  else {
-    res.redirect('/form')
-  }
-});
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
+    });
+
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -65,5 +75,9 @@ app.get('/deleteCookie', (req, res) => {
   res.send('cookie deleted');
 });
 
+app.get('/logout', (req, res)=>{
+  req.logout();
+  res.redirect('/')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
